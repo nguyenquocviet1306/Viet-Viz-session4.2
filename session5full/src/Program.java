c367e0d59235c3c18813f056bd643bed4b7afb32
nguyenquocviet1306 <nguyenquocviet1306@gmail.com>
2016-08-22 14:06:09 +0700

    Revert "session5"
    This reverts commit af312a820e7099d7ae286b34e549de17760486a2.

--- session5full/src/Program.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Viet on 7/24/2016.
- */
-public class Program {
-    public static void main(String[] args) {
-        System.out.println("Hello techkids");
-        GameWindow gameWindow = new GameWindow();
-    }
-}
